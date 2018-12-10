90185bc61da771e63c37fa16cac5f1b2c4609045
Elias Luhr <e.luhr@herzogkommunikation.de>
2018-12-10 08:01:59 +0100

    init

---
+++ assets/SwiperMinAsset.php
@@ -0,0 +1,33 @@
+<?php
+namespace eluhr\swiper\assets;
+
+
+use yii\web\AssetBundle;
+
+/**
+ * Class SwiperMinAsset
+ *
+ * @package romkaChev\yii2\swiper\assets
+ */
+class SwiperMinAsset extends AssetBundle
+{
+    /**
+     * @inheritdoc
+     */
+    public $sourcePath = '@bower/swiper/dist';
+
+    /**
+     * @inheritdoc
+     */
+    public $js = [
+        'js/swiper.min.js'
+    ];
+
+    /**
+     * @inheritdoc
+     */
+    public $css = [
+        'css/swiper.css',
+    ];
+
+}
